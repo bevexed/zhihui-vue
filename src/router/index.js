1e5353214c2f0da19e1e730875946e172f5d3554
bevexed <547551650@qq.com>
2019-01-25 10:57:09 +0800

    上线新地址

--- src/router/index.js
+++ src/router/index.js
@@ -156,19 +156,19 @@ router.beforeEach((to, from, next) => {
   *  2. 当用户被分享进来时，链接内部不存在 UId 所以 uid 为 null
   * */
 
-  // uidExist()
-  // let mid = getQuery('mid');
-  // if (mid !== 'null') {  // 一定是被分享进来的
-  //   if (!localStorage.uid) {
-  //     window.location.assign(`https://shop.zhihuimall.com.cn/app/index.php?i=1604&c=entry&mid=${mid}&do=shop&m=vslai_shop`) // 去拿授权
-  //   }
-  //   next()
-  // }
-  //
-  // if (!localStorage.uid) {
-  //   window.location.assign(`https://shop.zhihuimall.com.cn/app/index.php?i=1604&c=entry&mid=${mid}&do=shop&m=vslai_shop`) // 去拿授权
-  // }
-  window.location.assign(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6ae88e9a0dcb59b1&redirect_uri=${encodeURIComponent('https://shop.zhihuimall.com.cn/zhihuishop/zhihui-master/test/dist/index.html#/index')}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`)
+  uidExist()
+  let mid = getQuery('mid');
+  if (mid !== 'null') {  // 一定是被分享进来的
+    if (!localStorage.uid) {
+      window.location.assign(`https://shop.zhihuimall.com.cn/app/index.php?i=1604&c=entry&mid=${mid}&do=shop&m=vslai_shop`) // 去拿授权
+    }
+    next()
+  }
+
+  if (!localStorage.uid) {
+    window.location.assign(`https://shop.zhihuimall.com.cn/app/index.php?i=1604&c=entry&mid=${mid}&do=shop&m=vslai_shop`) // 去拿授权
+  }
+  // window.location.assign(`https://open.weixin.qq.com/connect/oauth2/authorize?appid=wx6ae88e9a0dcb59b1&redirect_uri=${encodeURIComponent('https://shop.zhihuimall.com.cn/zhihuishop/zhihui-master/test/dist/index.html#/index')}&response_type=code&scope=snsapi_userinfo&state=STATE#wechat_redirect`)
   next()
 });
 
